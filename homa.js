e09e81d566cef1ab565242feaa15bbf99873fef5
dzenvikas <vikaskumar.vsk@gmail.com>
2018-12-16 22:27:53 +0530

    type coercion, variable mutation

---
+++ homa.js
@@ -0,0 +1,66 @@
+/*************************
+ * Variables and Data types
+*/ 
+/*
+var firstName = 'Vivek';
+console.log(firstName);
+
+var lastName = 'Kumar';
+var age = 28;
+
+var fullAge = true;
+// console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+//Variable naming rules
+var _3years = 3;
+var JohnMark = 'John and Mark';
+// var if = 23; // reserved keyword
+*/
+
+
+/*************************
+ * Variable mutation and type coercion
+*/ 
+
+// Type coercion
+var firstName = 'John';
+var age = 28;
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+
+console.log(
+    firstName + ' is a ' + age + ' year old ' + job + 
+    '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(
+    firstName + ' is a ' + age + ' year old ' + job + 
+    '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
+
+
+
+
+
+
+
+
+
+
+
+
+
