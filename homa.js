1f5c8ef9c1b3986dbb77b8c6c42c7369324aba68
dzenvikas <vikaskumar.vsk@gmail.com>
2018-12-16 22:37:19 +0530

    basic operators

--- homa.js
+++ homa.js
@@ -29,6 +29,7 @@ var JohnMark = 'John and Mark';
 */ 
 
 // Type coercion
+/*
 var firstName = 'John';
 var age = 28;
 console.log(firstName + ' ' + age);
@@ -51,14 +52,38 @@ alert(
 
 var lastName = prompt('What is his last name?');
 console.log(firstName + ' ' + lastName);
+*/
 
 
+/*************************
+ * Basic Operators
+*/ 
 
+var now, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
 
+// Math operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
 
+console.log(yearJohn);
 
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
 
+// Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
 
+// Typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
 
 
 
