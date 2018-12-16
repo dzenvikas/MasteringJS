948d2516bff800c9c9321e5a58ae96b4fc1ae0f7
dzenvikas <vikaskumar.vsk@gmail.com>
2018-12-16 21:57:41 +0530

    variables and data types

---
+++ script.js
@@ -0,0 +1,20 @@
+/*************************
+ * Variables and Data types
+*/ 
+
+var firstName = 'Vivek';
+console.log(firstName);
+
+var lastName = 'Kumar';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+var _3years = 3;
