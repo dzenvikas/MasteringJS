6b986cd2e49041b4ef69ad9a4d1390d1ed65c441
dzenvikas <vikaskumar.vsk@gmail.com>
2018-12-18 21:09:28 +0530

    call, bind, apply

--- advance JS/advanceScript.js
+++ advance JS/advanceScript.js
@@ -124,7 +124,7 @@ game();
 /*************************
  * Closure
 */ 
-
+/*
 function retirement(retirementAge) {
     var a = ' years left until retirement.';
     return function(yearOfBirth) {
@@ -156,16 +156,52 @@ function interviewQuestion(job) {
 }
 
 interviewQuestion('teacher')('John');
+*/
 
 
+/*************************
+ * Bind, Call, apply
+*/ 
 
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal') {
+            console.log(
+                'Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' +
+                this.name + ', I\'m a ' + this.job + 'and I\'m ' +
+                this.age + ' years old.');
+        } else if (style === 'friendly') {
+            console.log(
+                'Hey! What\'s up? I\'m ' + this.name + ', I\'m a '+
+                this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' +
+                timeOfDay + ' .'
+            );
+        }
+    }
+}
 
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+}
 
+john.presentation('formal', 'morning');
 
+john.presentation.call(emily, 'friendly', 'afternoon'); // method borrowing
 
+john.presentation.apply(emily, ['friendly', 'afternoon']);
 
+var johnFriendly = john.presentation.bind(john, 'friendly'); // creates a copy with preset
 
+johnFriendly('morning');
+johnFriendly('night');
 
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
 
 
 
