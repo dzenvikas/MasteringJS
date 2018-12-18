05784c9fe8cc555406cb6d132fe65ee2e26b405c
dzenvikas <vikaskumar.vsk@gmail.com>
2018-12-18 06:48:36 +0530

    primitives vs objects

--- advance JS/advanceScript.js
+++ advance JS/advanceScript.js
@@ -1,6 +1,6 @@
 
 // Function constructor
-
+/*
 var john = {
     name: 'John',
     yearOfBirth: 1990,
@@ -28,14 +28,41 @@ var mark = new Person('Mark', 1948, 'retired');
 john.calcAge();
 jane.calcAge();
 console.log(jane);
+*/
+
+// Primitives vs objects
+
+var a = 23;
+var b = a; // reference to b
+a = 46;
+console.log(a);
+console.log(b);
+
+var obj1 = {
+    name: 'John',
+    age: 26
+}
+var obj2 = obj1; // reference to obj1
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
 
 
+// Functions
+var age = 27; // primitive
+var obj = {
+    name: 'Vikas',
+    city: 'Dhanbad'
+}
 
+function change(a, b) {
+    a = 30;
+    b.city = 'Ayodhya'
+}
 
-
-
-
-
+change(age, obj);
+console.log(age);
+console.log(obj.city);
 
 
 
