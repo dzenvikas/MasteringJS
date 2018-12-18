bb2d31da5decd223a078de73ec72de2aa1e76793
dzenvikas <vikaskumar.vsk@gmail.com>
2018-12-18 07:00:10 +0530

    first class functions

--- advance JS/advanceScript.js
+++ advance JS/advanceScript.js
@@ -31,7 +31,7 @@ console.log(jane);
 */
 
 // Primitives vs objects
-
+/*
 var a = 23;
 var b = a; // reference to b
 a = 46;
@@ -63,19 +63,39 @@ function change(a, b) {
 change(age, obj);
 console.log(age);
 console.log(obj.city);
+*/
 
 
+/*************************
+ * Function returning function
+*/ 
+
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function (name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function (name) {
+            console.log('What subject do you teach, ' + name + ' ?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello' + name + ', what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
 
 
+teacherQuestion('John');
+designerQuestion('Jane');
+designerQuestion('Mark');
+designerQuestion('Mike');
 
-
-
-
-
-
-
-
-
+interviewQuestion('teacher')('Vivek');
 
 
 
