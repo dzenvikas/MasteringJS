a9a1a18c63ec3fc428e51fed12a9045c0db3c503
dzenvikas <vikaskumar.vsk@gmail.com>
2018-12-18 08:03:16 +0530

    closures

--- advance JS/advanceScript.js
+++ advance JS/advanceScript.js
@@ -101,7 +101,7 @@ interviewQuestion('teacher')('Vivek');
 /*************************
  * IIFE
 */ 
-
+/*
 function game() {
     var score = Math.random*10;
     console.log(score >= 5);
@@ -118,16 +118,44 @@ game();
     var score = Math.random*10;
     console.log(score >= 5);
 })(5);
+*/
 
 
+/*************************
+ * Closure
+*/ 
 
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
 
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
 
+retirementUS(1994);
+retirementGermany(1994);
+retirementIceland(1994);
 
+//retirement(66)(1990);
 
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is ?');
+        } else if (job === 'designer') {
+            console.log('What subject do you teach, ' + name + ' ?');
+        } else {
+            console.log('Hello ' + name + ', what do you do ?');
+        }
+    }
+}
 
-
-
+interviewQuestion('teacher')('John');
 
 
 
