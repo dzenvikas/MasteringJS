b1a73684c1297e8b0b1cd0af2aac9e87b6f6969c
dzenvikas <vikaskumar.vsk@gmail.com>
2018-12-22 11:17:11 +0530

    string literals, new string methods: startsWith, endsWith, includes

--- ES6/script.js
+++ ES6/script.js
@@ -9,7 +9,7 @@
  * let and const are block scoped
  * while var is function scoped. Preety straight-forward, right ?
  */
-
+/*
 /////////////////////
 // a block
 {
@@ -37,12 +37,40 @@ var firstName = 'Vikas Kumar';
 })();
 
 console.log(c);
+*/
 
 
+/////////////////////
+// String literals
 
+let firstName = 'Vikas';    // Can be mutated
+let lastName = 'Kumar';
+const yearOfBirth = 1994;   // Can't be mutated
 
+function calcAge(birthYear){
+    const currentYear = new Date().getFullYear();
+    return currentYear - birthYear;
+}
 
+// // ES5
+// console.log(
+//     'This is ' + firstName + ' ' + lastName + 
+//     '. He was born in ' + yearOfBirth +
+//     '. Today, he is ' + calcAge(yearOfBirth) +
+//     ' years old.'
+//      );
 
+// ES6
+console.log(`This is ${firstName} ${lastName}.
+He was born in ${yearOfBirth}.
+Today, he is ${calcAge(yearOfBirth)} year old.`
+)
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('V'));
+console.log(n.endsWith('R'));
+console.log(n.includes(' '));
+console.log(n.includes('kum'));
 
 
 
